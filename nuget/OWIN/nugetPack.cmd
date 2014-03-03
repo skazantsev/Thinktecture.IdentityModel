214016e7f952651bf02c040304be6846b4089cf2
Dominick Baier <dbaier@leastprivilege.com>
2014-03-03 11:34:58 +0100

    Updated Owin package

--- nuget/OWIN/nugetPack.cmd
+++ nuget/OWIN/nugetPack.cmd
@@ -1,2 +1,4 @@
-xcopy ..\..\build\thinktecture.identitymodel.owin.* lib\net45 /y
-NuGet.exe pack Thinktecture.IdentityModel.Owin.nuspec -OutputDirectory ..\
+xcopy ..\..\build\thinktecture.identitymodel.owin.dll lib\net45 /y
+xcopy ..\..\build\thinktecture.identitymodel.owin.pdb lib\net45 /y
+
+NuGet.exe pack Thinktecture.IdentityModel.Owin.nuspec -exclude *.cmd -OutputDirectory ..\
