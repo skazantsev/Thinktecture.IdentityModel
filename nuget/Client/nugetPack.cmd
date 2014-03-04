786a2fce0d4e3d563218c03f7876bf148a63107b
Dominick Baier <dbaier@leastprivilege.com>
2014-03-04 17:48:37 +0100

    Updated Client nuget

--- nuget/Client/nugetPack.cmd
+++ nuget/Client/nugetPack.cmd
@@ -1,2 +1,4 @@
-xcopy ..\..\build\thinktecture.identitymodel.client*.* lib\portable-net45+wp80+win8 /y
+xcopy ..\..\build\thinktecture.identitymodel.client.dll lib\portable-net45+wp80+win8 /y
+xcopy ..\..\build\thinktecture.identitymodel.client.pdb lib\portable-net45+wp80+win8 /y
+
 NuGet.exe pack Thinktecture.IdentityModel.Client.nuspec -exclude *.cmd -OutputDirectory ..\
