d87d333a3e052c3aaa92f24b2b7b887505697f9e
Dominick Baier <dbaier@leastprivilege.com>
2014-06-01 15:59:16 +0200

    Added Web API plumbing for resource authorization

---
+++ source/Owin.ResourceAuthorization.WebApi/HttpRequestMessageExtensions.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see LICENSE
+ */
+
+using Microsoft.Owin;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Thinktecture.IdentityModel;
+using Thinktecture.IdentityModel.Owin.ResourceAuthorization;
+
+namespace System.Net.Http
+{
+    public static class HttpRequestMessageExtensions
+    {
+        public static Task<bool> CheckAccessAsync(this HttpRequestMessage request, string action, params string[] resources)
+        {
+            var authorizationContext = new ResourceAuthorizationContext(
+                request.GetOwinContext().Authentication.User ?? Principal.Anonymous,
+                action,
+                resources);
+
+            return request.CheckAccessAsync(authorizationContext);
+        }
+
+        public static Task<bool> CheckAccessAsync(this HttpRequestMessage request, IEnumerable<Claim> actions, IEnumerable<Claim> resources)
+        {
+            var authorizationContext = new ResourceAuthorizationContext(
+                request.GetOwinContext().Authentication.User ?? Principal.Anonymous,
+                actions,
+                resources);
+
+            return request.CheckAccessAsync(authorizationContext);
+        }
+
+        public static Task<bool> CheckAccessAsync(this HttpRequestMessage request, ResourceAuthorizationContext authorizationContext)
+        {
+            return request.CheckAccessAsync(authorizationContext);
+        }
+
+        private static async Task<bool> CheckAccessAsync(this IOwinContext context, ResourceAuthorizationContext authorizationContext)
+        {
+            return await context.GetAuthorizationManager().CheckAccessAsync(authorizationContext);
+        }
+
+        private static IResourceAuthorizationManager GetAuthorizationManager(this IOwinContext context)
+        {
+            var am = context.Get<IResourceAuthorizationManager>(ResourceAuthorizationManagerMiddleware.Key);
+
+            if (am == null)
+            {
+                throw new InvalidOperationException("No AuthorizationManager set.");
+            }
+
+            return am;
+        }
+    }
+}
---
+++ source/Owin.ResourceAuthorization.WebApi/ResourceAuthorizeAttribute.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see LICENSE
+ */
+
+using System;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace Thinktecture.IdentityModel.WebApi
+{
+    public class ResourceAuthorizeAttribute : AuthorizeAttribute
+    {
+        private string _action;
+        private string[] _resources;
+
+        public ResourceAuthorizeAttribute()
+        { }
+
+        public ResourceAuthorizeAttribute(string action, params string[] resources)
+        {
+            _action = action;
+            _resources = resources;
+        }
+
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            if (!string.IsNullOrWhiteSpace(_action))
+            {
+                return CheckAccess(actionContext.Request, _action, _resources);
+            }
+            else
+            {
+                var action = actionContext.ActionDescriptor.ActionName;
+                var resource = actionContext.ControllerContext.ControllerDescriptor.ControllerName;
+
+                return CheckAccess(actionContext.Request, action, resource);
+            }
+        }
+
+        protected virtual bool CheckAccess(HttpRequestMessage request, string action, params string[] resources)
+        {
+            var task = request.CheckAccessAsync(_action, _resources);
+            
+            if (task.Wait(5000))
+            {
+                return task.Result;
+            }
+            else
+            {
+                throw new TimeoutException();
+            }
+        }
+    }
+}
