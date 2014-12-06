b11c49e3c5ebe609c9a40e958bcffce7d4c930a7
Dominick Baier <dbaier@leastprivilege.com>
2014-12-06 10:36:34 +0100

    Updated nuspec and pack cmd

--- nuget/Core/nugetPack.cmd
+++ nuget/Core/nugetPack.cmd
@@ -1,2 +1,2 @@
 xcopy ..\..\build\thinktecture.identitymodel.core*.* lib\net45 /y
-NuGet.exe pack Thinktecture.IdentityModel.Core.nuspec -OutputDirectory ..\
+NuGet.exe pack Thinktecture.IdentityModel.Core.nuspec -exclude *.cmd -OutputDirectory ..\
